11d5d3dde40220df88103147b81b573c796b5d48
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-19 10:22:44 +0600

    making factory & db seeding

--- profile-backend/database/seeders/DatabaseSeeder.php
+++ profile-backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Course;
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Student::factory()->times(15)->create();
+        Course::factory()->times(8)->create()->each(function($course){
+             $course->students()->sync(
+                Student::all()->random(3)
+             );
+        });
     }
 }
