9a0f0e333c40fbfdf6ff0936164aeb5d7910d1f2
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-19 15:35:59 +0600

    usercontroller api test_10

---
+++ profile-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $inputs = $request->input();
+        $inputs["password"] = Hash::make(trim($request->password));
+        $e = User::create($inputs);
+         return response()->json([
+            'data'=>$e,
+            'message'=>"successfully created user.",
+         ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $e = User::find($id);
+        if(isset($e)){
+            return response()->json([
+                'data' => $e,
+                'message'=> "user found success",
+            ]);
+        }else {
+            return response()->json([
+                'error'=>true,
+                'message'=>"No User found",
+            ]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $e = User::find($id);
+        if(isset($e)){
+            $e->name =$request->name;
+            $e->email = $request->email;
+            $e->password = Hash::make($request->password);
+            //return $e->save();
+            if($e->save()){
+                return response()->json([
+                    'data'=>$e,
+                    'message'=> "successfully updated user",
+
+                ]);
+            }else{
+                return response()->json([
+                    'data'=>$e,
+                    'message'=>"the user was not updated",
+                ]);
+
+            }
+        }else{
+            return response()->json([
+                'error'=>true,
+                'message'=> "No exist user.",
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $e = User::find($id);
+        if(isset($e)){
+             $res=User::destroy($id);
+             if($res){
+                 return response()->json([
+                     'data'=>$e,
+                     'message'=>"user successfully eliminated",
+                 ]);
+             }else {
+                 return response()->json([
+                     'data'=>$e,
+                     'mensaje'=>"user doesn't exist",
+                 ]);
+             }
+        }else {
+         return response()->json([
+             'error'=>true,
+              'message'=>"user does not exist",
+
+         ]);
+
+        }
+    }
+}
--- profile-backend/routes/api.php
+++ profile-backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource("v1/students", StudentsController::class);
+Route::apiResource("v1/users", UserController::class);
+
