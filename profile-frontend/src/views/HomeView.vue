b2df4695ecae16272c14cb1bda16266b281df25e
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-20 11:30:35 +0600

    bs table set up-f5

--- profile-frontend/src/views/HomeView.vue
+++ profile-frontend/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="row">
+      <div class="col-lg-8 offset-lg-2">
+         <div class="table-responsive">
+
+          <table class="table table-bordered table-hover">
+         
+          </table>
+
+         </div>
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
