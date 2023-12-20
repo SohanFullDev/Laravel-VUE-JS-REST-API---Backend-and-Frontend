c68a8b0e7148efb6f08041765f8cc60eb4e9fdb1
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-20 16:36:57 +0600

    register form-a-f11

--- profile-frontend/src/views/StudentNew.vue
+++ profile-frontend/src/views/StudentNew.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="about">
-      <h1>New Student</h1>
-    </div>
+     <div class="row mt-3">
+        <div class="col-md-6 offset-md-3">
+           <div class="card">
+             <div class="card-header bg-dark text-white text-center">
+                Registar Student
+             </div>
+             <div class="card-body">
+         <form action="">
+       <div class="d-grid col-6 mx-auto mb-3">
+        <img v-if="this.photo" height="100" :src="this.photo" id="fotoimg" class="img-thumbnail" alt="">
+        <img v-else height="100" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-256.png" class="img-thumbnail" id="photoimg" alt="">
+
+       </div>
+               </form>
+
+             </div>
+           </div>
+        </div>
+
+     </div>
   </template>
   
