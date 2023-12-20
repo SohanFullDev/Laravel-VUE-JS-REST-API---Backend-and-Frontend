b3ec5e717f032407d0a465225429125fd9af8ea0
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-20 13:05:48 +0600

    bs table setting & api calling-f6

--- profile-frontend/src/views/HomeView.vue
+++ profile-frontend/src/views/HomeView.vue
@@ -4,7 +4,35 @@
          <div class="table-responsive">
 
           <table class="table table-bordered table-hover">
-         
+            <thead>
+              <tr>
+                 <th>#</th>
+                 <th>ID</th>
+                 <th>PHOTO</th>
+                 <th>NAME</th>
+                 <th>LASTNAME</th>
+                 <th>RECORD</th>
+                 <th></th>
+              </tr>
+            </thead>
+            <tbody class="table-group-divider" id="content">
+              <tr v-if="this.charging">
+                <td colspan="6">
+                  <h3>Charging......</h3>
+                </td>
+              </tr>
+              <tr v-else v-for="stu, i in this.students" :key="stu.id">
+                <td v-text="(i+1)"></td>
+                <td v-text="stu.id"></td>
+                <td v-text="stu.photo"></td>
+                <td v-text="stu.name"></td>
+                <td v-text="stu.lastname"></td>
+                <td v-text="stu.created_at"></td>
+                <td>
+
+                </td>
+              </tr>
+            </tbody>
           </table>
 
          </div>
@@ -13,6 +41,31 @@
 </template>
 
 <script>
+import axios from 'axios';
 
+export default{
+  data(){
+    return {
+      students:null,
+      charging:true
+    }
+  },
+  mounted(){
+    this.getStudents();
+  },
+  methods:{
+    getStudents(){
+      this.charging = true;
+      axios.get('http://localhost:8000/api/v1/students').then(
+        res =>{
+           this.students = res.data;
+           this.charging = false;
+        }
+      );
+
+    }
+  }
+
+}
 
 </script>
